719396b755a551303bfc803571fc18f8cd828828
Shahrier Hossain <hossainshahrier.shm@gmail.com>
2023-08-06 04:09:34 +0600

    Create Thread, Home page thread showing and backend

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,34 @@
-export default function Home() {
+import { ThreadCard } from '@/components/cards/ThreadCard';
+import { fetchThreads } from '@/lib/actions/thread.action';
+import { currentUser } from '@clerk/nextjs';
+
+export default async function Home() {
+  const results = await fetchThreads(1, 30);
+  const user = await currentUser();
   return (
     <div>
       <h1 className="head-text text-left">Home</h1>
+      <section className="mt-9 flex flex-col gap-10">
+        {results.threads.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {results.threads.map((thread) => (
+              <ThreadCard
+                key={thread._id}
+                id={thread._id}
+                currentUserId={user?.id || ''}
+                parentId={thread.parentId}
+                content={thread.text}
+                author={thread.author}
+                community={thread.community}
+                createdAt={thread.createdAt}
+                comments={thread.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </div>
   );
 }
--- lib/actions/thread.action.ts
+++ lib/actions/thread.action.ts
@@ -36,3 +36,79 @@ export async function createThread({
     throw new Error(`Error creating thread: ${error}`);
   }
 }
+
+export async function fetchThreads(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  // Calculate the number of posts to skip based on the page number and page size.
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  // Create a query to fetch the posts that have no parent (top-level threads) (a thread that is not a comment/reply).
+  const threadsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({
+      path: 'author',
+      model: User,
+    })
+    // .populate({
+    //   path: 'community',
+    //   model: Community,
+    // })
+    .populate({
+      path: 'children', // Populate the children field
+      populate: {
+        path: 'author', // Populate the author field within children
+        model: User,
+        select: '_id name parentId image', // Select only _id and username fields of the author
+      },
+    });
+
+  // Count the total number of top-level (threads) i.e., threads that are not comments.
+  const totalThreadsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  }); // Get the total count of threads
+
+  const threads = await threadsQuery.exec();
+
+  const isNext = totalThreadsCount > skipAmount + threads.length;
+
+  return { threads, isNext };
+}
+
+export async function fetchThreadById(id: string) {
+  connectToDB();
+
+  try {
+    // TODO: populate Community
+    const thread = await Thread.findById(id)
+      .populate({
+        path: 'author',
+        model: User,
+        select: '_id id name image',
+      })
+      .populate({
+        path: 'children', // Populate the children field
+        populate: [
+          {
+            path: 'author', // Populate the author field within children
+            model: User,
+            select: '_id id name parentId image', // Select only _id and username fields of the author
+          },
+          {
+            path: 'children', // Populate the children field
+            populate: {
+              path: 'author', // Populate the author field within children
+              model: User,
+              select: '_id id name parentId image', // Select only _id and username fields of the author
+            },
+          },
+        ],
+      })
+      .exec();
+    return thread;
+  } catch (error: any) {
+    throw new Error(`Error fetching thread: ${error.message}`);
+  }
+}
